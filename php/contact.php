6bd484ce99776037ff9c2be45d0a6c4dc58f4285
Taireru Benaru <38292283+chairdev@users.noreply.github.com>
2023-09-08 12:21:11 +0200

    add contact form css

---
+++ php/contact.php
@@ -0,0 +1,10 @@
+<?php 
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    //faya
+    $email_from = 'example@example.com';
+    
+?>
